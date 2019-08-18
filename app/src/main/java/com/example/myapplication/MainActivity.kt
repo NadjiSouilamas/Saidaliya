f6fb1eaacbfa1891f06dd9930e195681ebce6d33
Khadidja <fk_hadj_henni@esi.dz>
2019-08-18 16:36:50 +0100

    bottom navigation working

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,8 +1,18 @@
 package com.example.myapplication
 
+import android.content.res.Resources
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.myapplication.LocalStorage.RoomService
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.proximite_fragment.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,6 +21,40 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Context for RoomService
         RoomService.context = this
+
+        // NavController
+        val host: NavHostFragment = supportFragmentManager
+            .findFragmentById(R.id.my_nav_host_fragment) as NavHostFragment? ?: return
+
+        val navController = host.navController
+
+        // Setup BottomNav to navigate with navController
+        setupBottomNavMenu(navController)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+//            val dest: String = try {
+//                resources.getResourceName(destination.id)
+//            } catch (e: Resources.NotFoundException) {
+//                Integer.toString(destination.id)
+//            }
+
+            if ( bottom_nav_view.visibility == View.VISIBLE)
+                bottom_nav_view.visibility = View.GONE
+            else
+                bottom_nav_view.visibility = View.VISIBLE
+//            Toast.makeText(this@MainActivity, "Navigated to $dest",
+//                Toast.LENGTH_SHORT).show()
+//            Log.d("NavigationActivity", "Navigated to $dest")
+        }
+
+    }
+
+
+
+    private fun setupBottomNavMenu(navController: NavController) {
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_nav_view)
+        bottomNav?.setupWithNavController(navController)
     }
 }
